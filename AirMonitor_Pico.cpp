c153ef1eb0a96544a07bd3ac6515f2334d46ef71
Pavel Holánek <HolanekPavel@outlook.com>
2025-11-26 00:46:26 +0100

    scd-41

--- AirMonitor_Pico.cpp
+++ AirMonitor_Pico.cpp
@@ -11,6 +11,11 @@
 #include "Clock.h"
 #include "Log.h"
 
+#include "scd4x_i2c.h"
+#include "sensirion_common.h"
+#include "sensirion_i2c_hal.h"
+#include <inttypes.h>  // PRIx64
+
 #ifndef __DISPLAYTEST_H__
 #define __DISPLAYTEST_H__
 
@@ -88,6 +93,13 @@ void printLine(uint16_t x, uint16_t y, const wchar_t *wideStr)
     GFX_flush();
 }
 
+void convert_and_print_serial(uint16_t* serial_raw) { //scd41
+    uint64_t serial_as_int = 0;
+    sensirion_common_to_integer((uint8_t*)serial_raw, (uint8_t*)&serial_as_int,
+                                LONG_INTEGER, 6);
+    printf("0x%" PRIx64, serial_as_int);
+}
+
 void InitializeDisplay(uint16_t color)
 {
     // Initialize display
@@ -148,15 +160,110 @@ void vTaskIncrementPrint(void *pvParameters)
     }
 }
 
+void scd41() {
+    int16_t error = NO_ERROR;
+    sensirion_i2c_hal_init();
+    scd4x_init(SCD41_I2C_ADDR_62);
+
+    uint16_t serial_number[3] = {0};
+    sleep_ms(300);
+    // Ensure sensor is in clean state
+    error = scd4x_wake_up();
+    if (error != NO_ERROR) {
+        printf("error executing wake_up(): %i\n", error);
+    }
+
+    error = scd4x_stop_periodic_measurement();
+    if (error != NO_ERROR) {
+        printf("error executing stop_periodic_measurement(): %i\n", error);
+    }
+
+    error = scd4x_reinit();
+    if (error != NO_ERROR) {
+        printf("error executing reinit(): %i\n", error);
+    }
+
+    // Read out information about the sensor
+    error = scd4x_get_serial_number(serial_number, 3);
+    if (error != NO_ERROR) {
+        printf("error executing get_serial_number(): %i\n", error);
+        return;
+    }
+    printf("serial number: ");
+    convert_and_print_serial(serial_number);
+    printf("\n");
+    //
+    // If temperature offset and/or sensor altitude compensation
+    // is required, you should call the respective functions here.
+    // Check out the header file for the function definitions.
+    // Start periodic measurements (5sec interval)
+    error = scd4x_start_periodic_measurement();
+    if (error != NO_ERROR) {
+        printf("error executing start_periodic_measurement(): %i\n", error);
+        return;
+    }
+    //
+    // If low-power mode is required, switch to the low power
+    // measurement function instead of the standard measurement
+    // function above. Check out the header file for the definition.
+    //
+    bool data_ready = false;
+    uint16_t co2_concentration = 0;
+    int32_t temperature = 0;
+    int32_t relative_humidity = 0;
+    uint16_t repetition = 0;
+    for (repetition = 0; repetition < 50; repetition++) {
+        //
+        // Slow down the sampling to 0.2Hz.
+        //
+        sleep_ms(5000);
+        //
+        // If ambient pressure compensation during measurement
+        // is required, you should call the respective functions here.
+        // Check out the header file for the function definition.
+        error = scd4x_get_data_ready_status(&data_ready);
+        if (error != NO_ERROR) {
+            printf("error executing get_data_ready_status(): %i\n", error);
+            continue;
+        }
+        while (!data_ready) {
+            sleep_ms(1000);
+            error = scd4x_get_data_ready_status(&data_ready);
+            if (error != NO_ERROR) {
+                printf("error executing get_data_ready_status(): %i\n", error);
+                continue;
+            }
+        }
+        error = scd4x_read_measurement(&co2_concentration, &temperature,
+                                       &relative_humidity);
+        if (error != NO_ERROR) {
+            printf("error executing read_measurement(): %i\n", error);
+            continue;
+        }
+        //
+        // Print results in physical units.
+        printf("CO2 concentration [ppm]: %u\n", co2_concentration);
+        printf("Temperature [m°C] : %i\n", temperature);
+        printf("Humidity [mRH]: %i\n", relative_humidity);
+    }
+
+    return;
+}
+
 int main()
 {
     stdio_init_all();
+
+    scd41();
+
     initClock();
 
     sleep_ms(10000);
     LOG("Initializing");
     sleep_ms(2000);
     LOG("Initializing");
+
+    
 
     InitializeDisplay(FOREGROUND);
 
